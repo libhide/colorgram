7822f6f7a49493bd3d884e1d6bf4044004bef096
Ratik Sharma <Ratik96@gmail.com>
2017-07-05 03:12:40 +0530

    Add bottom fragment anims

--- app/src/main/java/com/ratik/colorgram/ColorPickerFragment.java
+++ app/src/main/java/com/ratik/colorgram/ColorPickerFragment.java
@@ -3,9 +3,13 @@ package com.ratik.colorgram;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatSeekBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Ratik on 05/07/17.
@@ -13,16 +17,27 @@ import android.view.ViewGroup;
 
 public class ColorPickerFragment extends Fragment {
 
+    @BindView(R.id.redSlider)
+    AppCompatSeekBar redSlider;
+    @BindView(R.id.greenSlider)
+    AppCompatSeekBar greenSlider;
+    @BindView(R.id.blueSlider)
+    AppCompatSeekBar blueSlider;
+
     public ColorPickerFragment() {
         // Empty constructor
+    }
+
+    public static ColorPickerFragment getInstance() {
+        return new ColorPickerFragment();
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.fragment_color_select, container, false);
+        ButterKnife.bind(this, rootView);
         return rootView;
     }
 }
--- app/src/main/java/com/ratik/colorgram/MainActivity.java
+++ app/src/main/java/com/ratik/colorgram/MainActivity.java
@@ -1,13 +1,61 @@
 package com.ratik.colorgram;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Constants
+    private boolean slidersAreVisible = false;
+
+    // Views
+    @BindView(R.id.mainLayout)
+    RelativeLayout mainLayout;
+
+    // Helpers
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private ColorPickerFragment colorPickerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        init();
+    }
+
+    private void init() {
+        fragmentManager = getSupportFragmentManager();
+        colorPickerFragment = ColorPickerFragment.getInstance();
+
+    }
+
+    public void toggleSliders(View view) {
+        if (slidersAreVisible) {
+            // hide slider fragment
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.bottom_up,
+                    R.anim.bottom_down);
+            fragmentTransaction.remove(colorPickerFragment);
+            fragmentTransaction.commit();
+            slidersAreVisible = false;
+        } else {
+            // show slider fragment
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.bottom_up,
+                    R.anim.bottom_down);
+            fragmentTransaction.add(R.id.slidersContainer, colorPickerFragment);
+            fragmentTransaction.commit();
+            slidersAreVisible = true;
+        }
     }
 }
