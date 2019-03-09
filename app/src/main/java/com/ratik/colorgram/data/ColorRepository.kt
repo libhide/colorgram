31625df679350571f9ebb2d8e9dfa0ea10a76ea4
Ratik Sharma <Ratik96@gmail.com>
2019-03-10 01:36:11 +0530

    Get rid of the redundant suspend keywords

--- app/src/main/java/com/ratik/colorgram/data/ColorRepository.kt
+++ app/src/main/java/com/ratik/colorgram/data/ColorRepository.kt
@@ -3,6 +3,6 @@ package com.ratik.colorgram.data
 import com.ratik.colorgram.model.GramColor
 
 interface ColorRepository {
-    suspend fun saveColor(color: GramColor)
-    suspend fun getColor(): GramColor
+    fun saveColor(color: GramColor)
+    fun getColor(): GramColor
 }
--- app/src/main/java/com/ratik/colorgram/data/ColorRepositoryImpl.kt
+++ app/src/main/java/com/ratik/colorgram/data/ColorRepositoryImpl.kt
@@ -5,13 +5,13 @@ import com.ratik.colorgram.model.GramColor
 
 class ColorRepositoryImpl(val localStorage: LocalStorage) : ColorRepository {
 
-    override suspend fun saveColor(color: GramColor) {
+    override fun saveColor(color: GramColor) {
         localStorage.putInt(PREF_RED, color.red)
         localStorage.putInt(PREF_GREEN, color.green)
         localStorage.putInt(PREF_BLUE, color.blue)
     }
 
-    override suspend fun getColor(): GramColor {
+    override fun getColor(): GramColor {
         val red = localStorage.getInt(PREF_RED, APP_RED)
         val green = localStorage.getInt(PREF_GREEN, APP_GREEN)
         val blue = localStorage.getInt(PREF_BLUE, APP_BLUE)
